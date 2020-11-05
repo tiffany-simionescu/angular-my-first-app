ecf6ac5b83354b0827f4b8f8765642e674aeaa85
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-11-04 21:12:46 -0500

    Finished all notes for 2nd project

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!'
+  serverName = "";
 
   constructor() {
     setTimeout(() => {
@@ -24,7 +25,12 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    // console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
