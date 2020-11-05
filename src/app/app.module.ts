4b8ffd6702eb5c703057184ad0f75017e099e837
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-11-05 00:23:10 -0500

    Assignment 2 - two-way binding, clear username

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
