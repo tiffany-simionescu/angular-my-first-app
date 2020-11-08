1dabc150a8d2b302fc9504b8f24f8143b630577c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-11-07 23:42:47 -0500

    Working through assignment, will update soon

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   // serverId = 10;
@@ -10,8 +15,16 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   // Method
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
